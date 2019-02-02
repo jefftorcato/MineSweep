c787d751543bcb0e8c0256266e7b714b35b4b4d2
jefftorcato <jefftorcato@gmail.com>
2019-02-02 19:18:48 +0530

    Some stuff

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener('click', function (event) {
         if (event.target.classList.contains('btn-difficulty')) {
             console.log(event.target.innerText);
+            document.getElementById('basediv').style.display = 'block';
+            document.getElementById('intro').style.display = 'none';
         }
     }, false);
 
