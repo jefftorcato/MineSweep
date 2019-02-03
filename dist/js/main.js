8a29b5adb064b1a1df4ff6c22380279170496281
jefftorcato <jefftorcato@gmail.com>
2019-02-03 22:00:43 +0530

    Some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,35 @@
+function closeWindow() {
+    var x = confirm('Exit Game?');
+    if (x) window.close();
+}
+
+function load(url, element) {
+    req = new XMLHttpRequest();
+
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Typical action to be performed when the document is ready:
+            element.innerHTML = req.responseText;
+        }
+    };
+
+    req.open("GET", url, true);
+    req.send(null);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+
+    let arr = [] // Global Variable Mine placement
 
     document.addEventListener('click', function (event) {
         let difficulty;
         if (event.target.classList.contains('btn-difficulty')) {
             difficulty = event.target.innerText;
             console.log(difficulty);
-            document.getElementById('basediv').style.display = 'block';
+            document.getElementById('basediv').style.display = 'flex';
+            document.getElementById('basediv').style.flexDirection = 'column';
+            document.getElementById('basediv').style.alignItems = 'center';
+            document.getElementById('basediv').style.justifyContent = 'center';
             document.getElementById('intro').style.display = 'none';
             init_size(difficulty);
         }
@@ -15,19 +39,19 @@ document.addEventListener("DOMContentLoaded", function () {
         let size = 0;
         switch (difficulty) {
             case "Easy":
-                size = 25;
+                size = 49;
                 console.log(size);
                 init_grid(size);
                 break;
 
             case "Medium":
-                size = 36;
+                size = 64;
                 console.log(size);
                 init_grid(size);
                 break;
 
             case "Hard":
-                size = 49;
+                size = 81;
                 console.log(size);
                 init_grid(size);
                 break;
@@ -42,23 +66,51 @@ document.addEventListener("DOMContentLoaded", function () {
         var i;
         let row_size;
 
-        if (size == 25) {
-            row_size = 5;
-        } else if (size == 36) {
-            row_size = 6;
-        } else {
+        if (size == 49) {
             row_size = 7;
+        } else if (size == 64) {
+            row_size = 8;
+        } else {
+            row_size = 9;
         }
 
         let root = document.documentElement;
         root.style.setProperty('--col-size', row_size);
         root.style.setProperty('--row-size', row_size);
-        
+
         for (i = 1; i <= size; i++) {
+
             var div = document.createElement("DIV");
-            div.setAttribute("id", size);
+            div.setAttribute("id", i);
+            div.setAttribute("name", i);
             div.setAttribute("class", "box");
             document.getElementById("wrapper").appendChild(div);
         }
+        assign_mines(size);
     }
+
+    function assign_mines(size) {
+
+        let mine_count = Math.floor((size / 100) * 40);
+
+        while (arr.length <= mine_count) {
+            var r = Math.floor(Math.random() * 100) + 1;
+            if (arr.indexOf(r) === -1 && r < size) arr.push(r);
+        }
+        console.log(arr);
+    }
+
+
+    document.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+        let here_flag = parseInt(event.target.getAttribute('name'));
+        
+        console.log(here_flag);
+        if(!isNaN(here_flag)){
+            var flag = document.createElement("IMG");
+            flag.setAttribute('src', './dist/img/flag.svg');
+            flag.setAttribute("class", "flag");
+            document.getElementById(here_flag).appendChild(flag);
+        }
+    }, false)
 });
